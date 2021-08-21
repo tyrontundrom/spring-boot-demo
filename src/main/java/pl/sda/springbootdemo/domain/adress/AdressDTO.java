5ab9144ad2c8eb784117de88d951441918260b58
ThinkPad <tyrontundrom@gmail.com>
2021-08-21 11:55:08 +0200

    add DTO & Mapper

---
+++ src/main/java/pl/sda/springbootdemo/domain/adress/AdressDTO.java
@@ -0,0 +1,14 @@
+package pl.sda.springbootdemo.domain.adress;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdressDTO {
+
+
+    private String city;
+    private String street;
+    private String postalCode;
+}
